da3473e4ada413e1f8dae38edd18206dc24add59
dianniii <kropuhinskaya@yandex.ru>
2025-07-09 21:25:48 +0300

    Обновление

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,8 +1,8 @@
 export const ROUTES = {
-    ABOUT: '/',
-    HOME: '/home',
-    TRAIN: '/train',
-    ADD_WORD: '/add_word',
-    WORDS: '/words',
-    NOT_FOUND: '*'
+    ABOUT: '/English-words/#/',
+    HOME: '/English-words/#/home',
+    TRAIN: '/English-words/#/train',
+    ADD_WORD: '/English-words/#/add_word',
+    WORDS: '/English-words/#/words',
+    NOT_FOUND: '/English-words/#/*'
 };
