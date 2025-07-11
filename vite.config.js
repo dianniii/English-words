2842f7cc196afbbc58f2111b2b1e246739e508cd
dianniii <kropuhinskaya@yandex.ru>
2025-07-11 12:26:26 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/English-words/',
-  plugins: [react()], 
-  server: {
-    port: 3000
-  }
+  plugins: [react()]
 })
